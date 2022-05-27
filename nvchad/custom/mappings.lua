54809989a7af830aca283574aec3bebbe0f7e754
llxlr <i@xhlr.top>
2022-05-28 00:29:32 +0800

    first commit

---
+++ nvchad/custom/mappings.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.coderunner = {
+
+  n = {
+    -- 使用 Ctrl+Alt+n 运行代码
+    ["<C-A-n>"] = { "<CMD>lua require'CodeRunner'.run()<CR>", "" },
+
+    -- 使用 Ctrl+` 开关浮动终端
+    ["<C-Space>"] = { "<CMD>lua require'CodeRunner'.show()<CR>", "" },
+    ["<C-Space>"] = { "<C-\\><C-n><CMD>lua require'CodeRunner'.hide()<CR>", "" }
+  },
+}
+
+return M
